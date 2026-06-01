8303736a821c74e2dac10c81939175357ff2a0fd
Kleiton camargo <kleiton_camargo@hotmail.com>
2026-06-01 14:08:46 -0300

    Create a new commit with the following message: ```feat: Add initial data generator script and database schema - Added `data_generator.py` to generate synthetic customer and account data using Faker. - Created `schema.sql` to define the database schema for customers and accounts. - Updated `docker-compose.yml` to include the data generator service.```

---
+++ postgres/schema.sql
@@ -0,0 +1,31 @@
+--DROP TABLE customer CASCADE;
+--DROP TABLE accounts CASCADE;
+DROP TABLE transactions CASCADE;
+
+CREATE TABLE customers(
+    customer_id SERIAL PRIMARY KEY,
+    customer_first_name VARCHAR(100),
+    customer_last_name VARCHAR(100),
+    customer_email VARCHAR(100),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE accounts (
+    account_id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES customers(customer_id) ON DELETE CASCADE,        
+    account_type VARCHAR(50) NOT NULL,
+    balance NUMERIC (18,2) NOT NULL DEFAULT 0 CHECK (balance >= 0),            
+    currency CHAR(3) NOT NULL DEFAULT 'USD',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+
+);
+
+CREATE TABLE transactions (
+    transactions_id BIGSERIAL PRIMARY KEY,        
+    account_id INT NOT NULL REFERENCES accounts(account_id) ON DELETE CASCADE,
+    txn_type VARCHAR(50) NOT NULL,            
+    amount NUMERIC (18,2) NOT NULL DEFAULT 0 CHECK (amount > 0),
+	related_account_id INT NULL,
+	status VARCHAR(50) NOT NULL DEFAULT 'COMPLETED',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
